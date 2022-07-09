2ba3653bd05515850670bf3811a9628b8ebb7dfb
Rich Kaht <adamselene1776@protonmail.com>
2022-07-09 13:09:35 -0600

    Amend comments to demonstrate understanding of index positions

--- arrays.rb
+++ arrays.rb
@@ -6,14 +6,14 @@ miles_run = [2.5, 3.2, 5.5, 1.5]
 
 run_this_day = [true, false, true, true]
 
-# This method will remove Ringo (because no one like Ringo anyway)
+# This method will remove Ringo because this string is the last element in the array at index position 3.
 beatles.pop
 
-# This method will add the element 9 to the end of the array
+# This method will add the element 9 to the end of the array in index position 4.
 apples.push (9)
 
-# This method will remove the first element of the array, 2.5
+# This method will remove the first element, index position 0, of the array, 2.5
 miles_run.shift
 
-# This method will add the element 5 to the beginning of the array
+# This method will add the element 5 to the beginning of the array in index position 0.
 run_this_day.unshift (5)
