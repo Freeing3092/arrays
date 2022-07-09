f5a08faa133015c680e7d53368509023a4884362
Rich Kaht <adamselene1776@protonmail.com>
2022-07-09 13:01:39 -0600

    Add one call for each method to the arrays

--- arrays.rb
+++ arrays.rb
@@ -5,3 +5,15 @@ apples = [5, 2, 7, 1]
 miles_run = [2.5, 3.2, 5.5, 1.5]
 
 run_this_day = [true, false, true, true]
+
+# This method will remove Ringo (because no one like Ringo anyway)
+beatles.pop
+
+# This method will add the element 9 to the end of the array
+apples.push (9)
+
+# This method will remove the first element of the array, 2.5
+miles_run.shift
+
+# This method will add the element 5 to the beginning of the array
+run_this_day.unshift (5)
